33583f5dacf04232b69305a32adde5934eb3b5b7
Diego Crespo <diego.crespo@grupoesfera.com.ar>
2022-09-10 18:20:16 -0300

    paso por parametros y dejo el usuario opcional, con nota

--- src/main/java/com/example/cashonline/controller/LoanController.java
+++ src/main/java/com/example/cashonline/controller/LoanController.java
@@ -5,11 +5,12 @@ import com.example.cashonline.service.LoanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/loans", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -18,15 +19,30 @@ public class LoanController {
     @Autowired
     LoanService loanService;
 
-    @GetMapping("{page}/{size}")
-    public List<LoanTO> getLoans(@PathVariable final int page, @PathVariable final int size ) {
-        return loanService.findAll(page, size);
+    @GetMapping()
+    public List<LoanTO> getLoans(@RequestParam final int page,
+                                 @RequestParam final int size,
+                                 @RequestParam(required = false) final Optional<Long> userId) {
+        return loanService.genericFind(page, size, userId);
     }
+   /*
+   Nota: Particularmente me parece mas prolijo que cada endpoint
+         tenga su set de parametros definidos, para no tomar decisiones
+         de que accion tomar.
+         Igual entiendo el punto del ejercicio.
+         Dejo abajo como lo habia hecho originalmente.
+   */
 
-    @GetMapping("{page}/{size}/{userId}")
-    public List<LoanTO> getLoansByUserId(@PathVariable final int page,
-                                         @PathVariable final int size,
-                                         @PathVariable final long userId) {
+   /*
+   public List<LoanTO> getLoans(@RequestParam final int page,
+                                 @RequestParam final int size) {
+        return loanService.findAll(page, size);
+
+    @GetMapping()
+    public List<LoanTO> getLoansByUserId(@RequestParam final int page,
+                                         @RequestParam final int size,
+                                         @RequestParam final long userId) {
         return loanService.findAllByUserId(page, size, userId);
     }
+    */
 }
--- src/main/java/com/example/cashonline/service/LoanService.java
+++ src/main/java/com/example/cashonline/service/LoanService.java
@@ -6,12 +6,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class LoanService {
 
     @Autowired
     LoanStore loanStore;
+
+    public List<LoanTO> genericFind(Integer page, Integer size, Optional<Long> userId) {
+
+        if(userId.isPresent()) {
+            return findAllByUserId(page, size, userId.get());
+        } else {
+           return findAll(page, size);
+        }
+    }
 
     public List<LoanTO> findAll(Integer page, Integer size) {
         return loanStore.getAllLoans(page, size);
