181d17044e941a9c68c78dd98ff7d7a68f6adfe7
Diego Crespo <diego.crespo@grupoesfera.com.ar>
2022-09-10 13:48:08 -0300

    agrega test y algunas funciones mas a user service

---
+++ README.md
@@ -0,0 +1,7 @@
+** TODO (no priorizado) 
+-- Mapper
+-- Excepciones
+-- Test (de service y store)
+-- Loans sin paginado por id
+-- Logs
+-- Paginado
--- src/test/java/com/example/cashonline/service/UserServiceTest.java
+++ src/test/java/com/example/cashonline/service/UserServiceTest.java
@@ -1,4 +1,57 @@
 package com.example.cashonline.service;
 
+import com.example.cashonline.persistence.store.UserStore;
+import com.example.cashonline.pojo.api.user.UserTO;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+
+@Test
 public class UserServiceTest {
+
+    @InjectMocks
+    private UserService userService;
+
+    @Mock
+    private UserStore userStore;
+
+    @BeforeClass
+    public void beforeClass() {
+
+        MockitoAnnotations.openMocks(this);
+        reset(userStore);
+    }
+
+    @DataProvider
+    public Object[][] dataProvider() {
+        UserTO userTO = new UserTO();
+        userTO.setFirstName("pepe");
+        userTO.setLastName("argento");
+        return new Object[][]{{userTO}};
+    }
+
+    @Test
+    public void getUser() {
+        userService.findById(1);
+        verify(userStore, times(1)).findById(1);
+    }
+
+    @Test(dataProvider = "dataProvider")
+    public void createUser(final UserTO userTO) {
+        userService.createUSer(userTO);
+        verify(userStore, times(1)).createUSer(userTO);
+    }
+
+    @Test
+    public void deleteUser() {
+        userService.deleteUserById(1);
+        verify(userStore, times(1)).deleteUserById(1);
+    }
 }
